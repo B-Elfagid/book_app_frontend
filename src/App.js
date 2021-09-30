552e563447f5303fc08cfcf9fd4a534e048b569b
Betty <betty_elfagid@yahoo.co.uk>
2021-09-30 23:15:07 +0100

    book component added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 import Book from './Book';
 function App() {
 
-   const read = {title: "Born a crime", author: "Trevor Noah"}
-
+   const books = [{image: "https://www.amazon.co.uk/images/I/6133MCTwt5L.jpg", title: "Born a crime", author: "Trevor Noah", likes: "8"}, {image: "https://www.amazon.co.uk/images/I/517pfctTa9L.jpg", title: "The Alchemist", author: "Paulo Coelho", likes: "6"}]
+    const booksLis = books.map((books, i) => <li key={i}>{books.title}</li>)
+    const bookComponents = books.map((books, i) => <li key={i}><Book {...books}/></li>)
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Books</h1>
         <img src={logo} className="App-logo" alt="logo" />
   
-        <Book {...read} />
+        {bookComponents}
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,34 @@
 
 
-function Book(props) {
-  return (
-<div>
-    <h1>Book List</h1>
-    <p>{props.title}</p>
-    <p>{props.author}</p>
-</div>
-  )
+// function Book(props) {
+//   return (
+// <div>
+//     <h1>Book List</h1>
+//     <p>{props.title}</p>
+//     <p>{props.author}</p>
+// </div>
+//   )
+// }
+
+// export default Book;
+
+import React from 'react';
+
+class Book extends React.Component {
+   render() {
+     return (
+       <div className="book" id={`book-${this.props.id}`}>
+        
+         <img src={this.props.image} alt="book image" />
+         <p>{this.props.title}</p>
+         <p>{this.props.author}</p>
+         <p>{this.props.likes}</p>
+         <button class="like-btn">Like &lt;3 </button>
+    
+        </div>
+     )
+    
+   }
 }
 
 export default Book;
